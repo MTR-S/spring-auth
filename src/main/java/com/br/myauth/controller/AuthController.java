8c6ea56f8702047470219d32f3863339d2e5c1e9
matheus-sortech <matheus.almeida@sortech.com.br>
2025-04-30 11:47:44 -0300

    feat: Api response annotations for deal with responseCode (200, 401, 404, 500). Added loginService.

--- src/main/java/com/br/myauth/controller/AuthController.java
+++ src/main/java/com/br/myauth/controller/AuthController.java
@@ -1,30 +1,50 @@
 package com.br.myauth.controller;
 
+import com.br.myauth.dto.ApiErrorResponse;
 import com.br.myauth.dto.LoginRequest;
 import com.br.myauth.dto.LoginResponse;
 import com.br.myauth.dto.SignupRequest;
+import com.br.myauth.entity.LoginAttemptEntity;
+import com.br.myauth.service.LoginService;
 import com.br.myauth.service.UserService;
 import com.br.myauth.utils.JwtHelper;
+
 import jakarta.validation.Valid;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping( "/api/auth/")
 public class AuthController {
     private final UserService userService;
+    private final LoginService loginService;
 
     private final AuthenticationManager authenticationManager;
 
-    public AuthController(UserService userService, AuthenticationManager authenticationManager) {
+    public AuthController(UserService userService,
+                          AuthenticationManager authenticationManager,
+                          LoginService loginService) {
         this.userService = userService;
+        this.loginService = loginService;
         this.authenticationManager = authenticationManager;
     }
 
+    @Operation(summary = "Authenticate user and return token")
+    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = LoginResponse.class)))
+    @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
     @PostMapping("/signup")
     public ResponseEntity<Void> signup(@Valid @RequestBody SignupRequest requestDto) {
 
@@ -33,14 +53,39 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "Authenticate user and return token")
+    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = LoginResponse.class)))
+    @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
     @PostMapping(value = "/login")
     public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
 
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.email(), request.password()));
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.email(), request.password()));
+        } catch (BadCredentialsException e) {
+            loginService.addLoginAttempt(request.email(), false);
+            throw e;
+        }
 
         String token = JwtHelper.generateToken(request.email());
+
+        loginService.addLoginAttempt(request.email(), true);
 
         return ResponseEntity.ok(new LoginResponse(request.email(), token));
     }
 
+    @Operation(summary = "Get recent login attempts")
+    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = LoginResponse.class)))
+    @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ApiErrorResponse.class)))
+    @GetMapping(value = "/loginAttempts")
+    public ResponseEntity<List<LoginAttemptEntity>> loginAttempts(@RequestHeader("Authorization") String token) {
+        String email = JwtHelper.extractUsername(token.replace("Bearer ", ""));
+
+        Optional<List<LoginAttemptEntity>> loginAttempts = loginService.findRecentLoginAttempts(email);
+
+        return loginAttempts.map(ResponseEntity::ok)
+                    .orElseGet(() -> ResponseEntity.status(HttpStatus.NO_CONTENT).build());
+    }
 }
